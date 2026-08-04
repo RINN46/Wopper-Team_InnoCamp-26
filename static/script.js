250b2e4504a42e5dcc721fc7d555e4a354f6fbf5
yaros <yaroslavostrecov16@gmail.com>
2026-08-04 11:10:53 +0300

    smth

--- static/script.js
+++ static/script.js
@@ -9,8 +9,41 @@ async function click_register(){
     const response = await fetch(`/reg?login=${login}&password=${password}&name=${name}&stack=${stack}`, {method: "POST"});
     const data = await response.json()
     if (data.OK){
-
+        location.href = "/main_page"
     }else{
         console.log("loh")
+        if (response.error === 1){
+            alert("Incorrect login")
+        } else if (response.error === 2){
+            alert("Incorrect password")
+
+        } else if (response.error === 3){
+            alert("Incorrect name")
+
+        } else if (response.error === 4){
+            alert("Incorrect stack")
+        }
     }
 }
+
+async function click_login(){
+    console.log("try to login")
+    const login = document.getElementById("login").textContent
+    const password = document.getElementById("password").textContent
+
+
+    const response = await fetch(`/reg?login=${login}&password=${password}`, {method: "POST"});
+    const data = await response.json()
+    if (data.OK){
+        location.href = "/main_page"
+    }else{
+        console.log("loh")
+        if (response.error === 1){
+            alert("Incorrect password")
+        } else if (response.error === 2){
+            alert("Incorrect login")
+
+        }
+    }
+}
+
